860550d19e5e02488e4a8407ebe929c0b83de6dc
Russkikh Evgenij <55288044+rrrrusich@users.noreply.github.com>
2021-07-03 16:52:11 +0500

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,225 +1,153 @@
-const project_folder = require("path").basename(__dirname);
-const source_folder = "src";
-
+let fileswatch = 'html,htm,txt,json,md,woff2'  //Файлы, при изменениях которых будет происходить жесткая перезагрузка страницы в браузере
+const prjFolder = require("path").basename(__dirname); 		// Дирректория с готовой версткой
+const srcFolder = "app";																	// Рабочая дирректория
 const path = {
 	build: {
-		html: project_folder + "/",
-		css: project_folder + "/css/",
-		js: project_folder + "/js/",
-		img: project_folder + "/images/",
-		fonts: project_folder + "/fonts/",
-		resources: project_folder + "/resources/",
+		html: `${prjFolder}/`,
+		css: `${prjFolder}/css/`,
+		js: `${prjFolder}/js/`,
+		img: `${prjFolder}/images/`,
+		fonts: `${prjFolder}/fonts/`,
+		resources: `${prjFolder}/resources/`,
 	},
 	src: {
-		html: source_folder + "/*.html",
-		css: source_folder + "/scss/style.scss",
-		js: source_folder + "/js/main.js",
-		svg: source_folder + "/images/icons/**/*.svg",
-		img: [source_folder + "/images/**/*.{jpg,jpeg,png,svg,gif,ico,webp}", "!" + source_folder + "/images/icons/**/*.svg"],
-		fonts: source_folder + "/fonts/*.ttf",
-		resources: source_folder + "/resources/**/**",
+		html: `${srcFolder}/*.html`,
+		css: `${srcFolder}/styles/main.scss`,
+		js: `${srcFolder}/js/*.js`,
+		svg: `${srcFolder}/images/icons/**/*.svg`,
+		img: `${srcFolder}/images/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
+		fonts: `${srcFolder}/fonts/*.ttf`,
+		resources: `${srcFolder}/resources/**/**`,
 	},
 	watch: {
-		html: source_folder + "/**/*.html",
-		css: source_folder + "/scss/**/*.scss",
-		js: source_folder + "/js/**/*.js",
-		svg: source_folder + "/images/icons/**/*.svg",
-		img: source_folder + "/images/**/*.{jpg,jpeg,png,svg,gif,ico,webp}",
-		resources: source_folder + "/resources/**/**",
+		html: `${srcFolder}/**/*.html`,
+		css: `${srcFolder}/styles/**/*.scss`,
+		js: `${srcFolder}/js/**/*.js`,
+		svg: `${srcFolder}/images/icons/**/*.svg`,
+		img: `${srcFolder}/images/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
+		resources: `${srcFolder}/resources/**/**`,
 	},
-	clean: ["./" + project_folder + "/", "!" + project_folder + "/{images, resources}/"],
-};
-const { src, dest, parallel, series, watch } = require("gulp");
-const gulp = require("gulp");
-const browsersync = require("browser-sync").create();
-const fileinclude = require("gulp-file-include");
-const notify = require("gulp-notify");
-const del = require("del");
-const sass = require("gulp-sass");
-const sourcemaps = require("gulp-sourcemaps");
-const shorthand = require("gulp-shorthand");
-const autoprefixer = require("gulp-autoprefixer");
-const group_media = require("gulp-group-css-media-queries");
-const clean_css = require("gulp-clean-css");
-const rename = require("gulp-rename");
-const uglify = require("gulp-uglify-es").default;
-const babel = require("gulp-babel");
-const imagemin = require("gulp-imagemin");
-const newer = require("gulp-newer");
-const ttf2woff = require("gulp-ttf2woff");
-const ttf2woff2 = require("gulp-ttf2woff2");
-const svgSprite = require("gulp-svg-sprite");
-const fonter = require("gulp-fonter");
+	clean: [`./${prjFolder}/`, `!${prjFolder}/images/ || !${prjFolder}/resouces/`]
+}
+
+const { src, dest, parallel, series, watch } = require('gulp');
+const browserSync = require('browser-sync').create();
+const bssi = require('browsersync-ssi');
+const ssi = require('ssi');
+const del = require('del');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const webpack = require('webpack-stream');
+const changed = require('gulp-changed');
+const imagemin = require('gulp-imagemin');
+const ttf2woff2 = require('gulp-ttf2woff2');
+
+
 
 // Browser-Sync
 const server = () => {
-	browsersync.init({
+	browserSync.init({
 		server: {
-			baseDir: "./" + project_folder + "/",
+			baseDir: `./${prjFolder}/`,
+			middleware: bssi({ baseDir: `./${prjFolder}/`, ext: '.html' })
 		},
 		notify: false,
 		online: true,
-		ui: false,
+		//tunnel: 'rusichsitename', 	/*  Использовать URL https://rusichsitename.loca.lt  */
 		browser: "chrome",
 	});
 };
 exports.server = server;
 
-// Resources
-const resources = () => {
-	return src(path.src.resources)
-		.pipe(newer(path.build.resources))
-		.pipe(dest(path.build.resources))
-		.pipe(browsersync.stream());
-};
-exports.resources = resources;
-
 // HTML
-const html = () => {
-	return src(path.src.html)
-		.pipe(fileinclude({
-			prefix: '@',
-			basepath: '@file'
-		}))
-		.pipe(dest(path.build.html))
-		.pipe(browsersync.stream());
+async function html() {
+	let includes = new ssi(`./${srcFolder}/`, `./${prjFolder}/`, `/**/*.html`)
+	includes.compile()
+	del(`${path.build.html}/html`, { force: true })
 };
-exports.html = html;
 
 // Styles
 const scss = () => {
 	return src(path.src.css)
 		.pipe(sourcemaps.init())
-		.pipe(sass({
-			outputStyle: "expanded",
-		}).on('error', notify.onError()))
-		.pipe(group_media())
-		.pipe(shorthand())
-		.pipe(autoprefixer({
-			overrideBrowserslist: ["last 5 versions"],
-			cascade: false,
-			grid: true,
-		}))
-		.pipe(dest(path.build.css))
-		.pipe(clean_css({
-			level: 2
-		}))
-		.pipe(rename({
-			extname: ".min.css",
-		}))
+		.pipe(sass())
+		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+		.pipe(cleanCSS({ level: { 1: { specialComments: 1 } },/* format: 'beautify' */ }))
+		.pipe(rename({ suffix: ".min" }))
 		.pipe(sourcemaps.write('.'))
 		.pipe(dest(path.build.css))
-		.pipe(browsersync.stream())
+		.pipe(browserSync.stream())
 };
 exports.scss = scss;
 
-// Scripts
-const js = () => {
+//Scripts
+function js() {
 	return src(path.src.js)
-		.pipe(fileinclude({
-			prefix: '@',
-			basepath: '@file'
-		}))
-		.pipe(
-			babel({
-				presets: ["@babel/preset-env"],
-			})
-		)
+		.pipe(webpack({
+			mode: 'production',
+			performance: { hints: false },
+			module: {
+				rules: [
+					{
+						test: /\.(js)$/,
+						exclude: /(node_modules)/,
+						loader: 'babel-loader',
+						query: {
+							presets: ['@babel/env'],
+							plugins: ['babel-plugin-root-import']
+						}
+					}
+				]
+			}
+		})).on('error', function handleError() {
+			this.emit('end')
+		})
+		.pipe(rename('main.min.js'))
 		.pipe(dest(path.build.js))
-		.pipe(uglify())
-		.pipe(
-			rename({
-				extname: ".min.js",
-			})
-		)
-		.pipe(dest(path.build.js))
-		.pipe(browsersync.stream());
-};
+		.pipe(browserSync.stream())
+}
 exports.js = js;
 
-// Images
+//Images
 const images = () => {
 	return src(path.src.img)
-		.pipe(newer(path.build.img))
+		.pipe(changed(path.build.img))
 		.pipe(
 			imagemin({
 				progressive: true,
 				svgoPlugins: [{ removeViewBox: false }],
 				interlaced: true,
-				optimizationLevel: 3, // от 0 до 7
+				optimizationLevel: 3, 													// 0 to 7
 			})
 		)
 		.pipe(dest(path.build.img))
-		.pipe(browsersync.stream());
+		.pipe(browserSync.stream());
 };
 exports.images = images;
-
-const svgSprites = () => {
-	return src(path.src.svg)
-		.pipe(svgSprite({
-			mode: {
-				stack: {
-					sprite: "../sprite.svg", // Имя итогового файла
-					example: true,
-				},
-			},
-		}))
-		.pipe(dest(path.build.img));
-}
-exports.svgSprites = svgSprites;
 
 // Fonts
 const fonts = () => {
 	src(path.src.fonts)
-		.pipe(ttf2woff())
-		.pipe(dest(path.build.fonts));
-	return src(path.src.fonts)
 		.pipe(ttf2woff2())
 		.pipe(dest(path.build.fonts));
 };
 exports.fonts = fonts;
 
-const otf2ttf = () => {
-	return src([source_folder + "/fonts/*.otf"])
-		.pipe(
-			fonter({
-				formats: ["ttf"],
-			})
-		)
-		.pipe(dest(source_folder + "/fonts/"));
+//Resources
+const resources = () => {
+	return src(path.src.resources)
+		.pipe(changed(path.build.resources))
+		.pipe(dest(path.build.resources))
+		.pipe(browserSync.stream());
 };
-exports.otf2ttf = otf2ttf;
-
-const fs = require("fs");
-const fontStyle = () => {
-	let file_content = fs.readFileSync(source_folder + "/scss/fonts.scss");
-	if (file_content == "") {
-		fs.writeFile(source_folder + "/scss/fonts.scss", "", cb);
-		return fs.readdir(path.build.fonts, function (err, items) {
-			if (items) {
-				let c_fontname;
-				for (let i = 0; i < items.length; i++) {
-					let fontname = items[i].split(".");
-					fontname = fontname[0];
-					if (c_fontname != fontname) {
-						fs.appendFile(
-							source_folder + "/scss/fonts.scss",
-							'@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n',
-							cb
-						);
-					}
-					c_fontname = fontname;
-				}
-			}
-		});
-	}
-};
-const cb = () => { }
-exports.fontStyle = fontStyle;
+exports.resources = resources;
 
 // Clean
 const clean = () => {
-	return del(path.clean);
+	return del(path.clean, { force: true })
 };
 exports.clean = clean;
 
@@ -228,12 +156,13 @@ const watchFils = () => {
 	watch([path.watch.html], { usePolling: true }, html);
 	watch([path.watch.css], { usePolling: true }, scss);
 	watch([path.watch.js], { usePolling: true }, js);
-	watch([path.watch.svg], { usePolling: true }, svgSprites);
 	watch([path.watch.img], { usePolling: true }, images);
 	watch([path.watch.resources], { usePolling: true }, resources);
+	watch(`${prjFolder}/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload);
 };
+exports.watchFils = watchFils;
 
-const build = series(clean, parallel(js, scss, html, svgSprites, images, fonts, resources), fontStyle);
+const build = series(clean, parallel(html, scss, js, resources, images, fonts));
 const Watch = parallel(build, watchFils, server);
 
 exports.build = build;
